7fb042b3bf56bb5c467c70fb6039bffee8f6bf72
deeptigupta222 <deeptig22@navgurukul.org>
2023-06-26 01:09:33 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById("start").addEventListener("click", function () {
+    makeSound();
+    let p1 = Math.floor(Math.random() * 6) + 1;
+    let p2 = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("p1").src = `assests/${p1}.png`;
+    document.getElementById("p2").src = `assests/${p2}.png`;
+    if (p1 > p2) {
+      document.querySelector("h1").textContent = "🏆🥳 Player 1 wins 🏆🥳";
+    } else if (p1 < p2) {
+      document.querySelector("h1").textContent = "🏆🥳 Player 2 wins 🏆🥳";
+    } else {
+      document.querySelector("h1").textContent = "😢 Oops! It's a DRAW 😢";
+    }
+  });
+  
+  function makeSound() {
+    let audio = new Audio("assests/sound.mp3"); //for sound
+    audio.play();
+  }
+
+
+
+  
